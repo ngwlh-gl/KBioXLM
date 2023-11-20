1ad167608a5b3c21ad9f47fbe909afe9c96551a2
耿磊 <84009553+ngwlh-gl@users.noreply.github.com>
2023-11-20 14:27:39 +0800

    Update run_gad.sh

--- downstream/RE/src/run_gad.sh
+++ downstream/RE/src/run_gad.sh
@@ -4,18 +4,18 @@ export CUDA_VISIBLE_DEVICES=1
 export LR=3e-5
 export EPOCH=20
 export SEED=42
-for MODEL in KBioXLM_model
+for MODEL in KBioXLM
 do
 for BS in 16
 do
 
-export MODEL_PATH=../../models/$MODEL
+export MODEL_PATH=ngwlh/KBioXLM
 
 # task=GAD_hf_zh_en
 # datadir=../data/seqcls/$task
 # outdir=runs/$task/$MODEL-$LR-$EPOCH-$SEED-$BS
 # mkdir -p $outdir
-# python3 -u seqcls/run_seqcls.py --model_name_or_path $MODEL_PATH --use_our_pretrain \
+# python3 -u seqcls/run_seqcls.py --model_name_or_path $MODEL_PATH \
 #   --train_file $datadir/train.json --validation_file $datadir/dev.json --test_file $datadir/test_en.json \
 #   --do_train --do_eval --do_predict --metric_name PRF1 \
 #   --per_device_train_batch_size 32 --gradient_accumulation_steps 1 --fp16 \
@@ -28,7 +28,7 @@ task=GAD_hf_en_zh
 datadir=../data/seqcls/$task
 outdir=runs/$task/$MODEL-$LR-$EPOCH-$SEED-$BS
 mkdir -p $outdir
-python3 -u seqcls/run_seqcls.py --model_name_or_path $MODEL_PATH --use_our_pretrain \
+python3 -u seqcls/run_seqcls.py --model_name_or_path $MODEL_PATH \
   --train_file $datadir/train.json --validation_file $datadir/dev.json --test_file $datadir/test_zh.json \
   --do_train --do_eval --do_predict --metric_name PRF1 \
   --per_device_train_batch_size 32 --gradient_accumulation_steps 1 --fp16 \
